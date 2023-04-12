c73730dedb9be60bba330d3105b1e7a8fbff166c
Filoe31 <refiloeseeletsa@gmail.com>
2023-04-12 12:28:55 +0200

    html form

---
+++ script.js
@@ -0,0 +1,17 @@
+function getFormValue()
+{
+    let Name = document.getElementById("Name").value ;
+    let Surname = document.getElementById("Surname").value ;
+    let Email = document.getElementById("Email").value;
+    let Post = document.getElementById("Post").value;
+
+    console.log(`
+    Name :${"Name"} \n
+    Surname : ${"Surname"}\n
+    Email : ${"Email"}\n
+    Post : ${"Post"}\n
+    `);
+    
+
+    
+} 
